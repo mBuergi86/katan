3e595df156ed2095f7385438fa98234a69c048cf
Markus Bürgi <117123240+mBuergi86@users.noreply.github.com>
2024-05-13 09:44:53 +0200

    refactor: ♻️ Add rowSumOfOddNumbers function to practice002.js
    Add the rowSumOfOddNumbers function to the practice002.js file to calculate the sum of the odd numbers in a given row.

---
+++ src/practice002/practice002.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rowSumOfOddNumbers = void 0;
+const rowSumOfOddNumbers = (n) => {
+    return Math.pow(n, 3);
+};
+exports.rowSumOfOddNumbers = rowSumOfOddNumbers;
+console.log((0, exports.rowSumOfOddNumbers)(1)); // 1
+console.log((0, exports.rowSumOfOddNumbers)(2)); // 8
+console.log((0, exports.rowSumOfOddNumbers)(42)); // 74088
